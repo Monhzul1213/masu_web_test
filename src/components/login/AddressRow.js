2e09d2368c8dd0ef61a97705be1008cae0a06068
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-17 12:09:06 +0800

    10.17_signup

--- src/components/login/AddressRow.js
+++ src/components/login/AddressRow.js
@@ -6,7 +6,7 @@ import { DynamicAIIcon } from '../all';
 const { Option } = Select;
 
 function AddressSelect(props){
-  const { data, value, setValue, text } = props;
+  const { data, value, setValue, text, placement } = props;
 
   const handleChange = e => {
     setValue({ value: e });
@@ -19,11 +19,16 @@ function AddressSelect(props){
   return (
     <Select
       className='addr_select'
+      placement={placement}
+      showSearch
+      filterOption={(input, option) => option.children?.toLowerCase().indexOf(input.toLowerCase()) >= 0}
       onChange={handleChange}
       value={value?.value}
       placeholder={text}
+      dropdownMatchSelectWidth={356}
+      dropdownAlign={placement ? {} : {offset: [-118, 5]}}
       suffixIcon={<DynamicAIIcon name='AiFillCaretDown' />}>
-      {data?.map(renderOption)}
+      {data?.map(renderOption)}  
     </Select>
   );
 }
@@ -33,9 +38,9 @@ export function AddressRow(props){
   const { text, addr1, addr2, addr3, setAddr1, setAddr2, setAddr3, addr1List, addr2List, addr3List } = props;
 
   const style = addr1?.error ? { borderColor: '#e41051', color: '#e41051' } : {};
-  const addr1Props = { data: addr1List, value: addr1, setValue: setAddr1, text: t('login.addr1') };
+  const addr1Props = { data: addr1List, value: addr1, setValue: setAddr1, text: t('login.addr1'), placement: 'bottomLeft' };
   const addr2Props = { data: addr2List, value: addr2, setValue: setAddr2, text: t('login.addr2') };
-  const addr3Props = { data: addr3List, value: addr3, setValue: setAddr3, text: t('login.addr3') };
+  const addr3Props = { data: addr3List, value: addr3, setValue: setAddr3, text: t('login.addr3'), placement: 'bottomRight' };
 
   return (
     <div className='input_container'>
--- src/helpers/dummyData.js
+++ src/helpers/dummyData.js
@@ -1,5 +1,5 @@
 export const addr1List = [
-  { value: 'Хот1', label: 'Хот1' },
+  { value: 'Хот1', label: 'Хотийн нэр 1' },
   { value: 'Хот2', label: 'Хот2' },
   { value: 'Хот3', label: 'Хот3' },
   { value: 'Хот4', label: 'Хот4' },
@@ -8,7 +8,7 @@ export const addr1List = [
 
 export const addr2List = [
   { value: 'Дүүрэг1', label: 'Дүүрэг1' },
-  { value: 'Дүүрэг2', label: 'Дүүрэг2' },
+  { value: 'Дүүрэг2', label: 'Дүүргийн нэр 2' },
   { value: 'Дүүрэг3', label: 'Дүүрэг3' },
   { value: 'Дүүрэг4', label: 'Дүүрэг4' },
   { value: 'Дүүрэг5', label: 'Дүүрэг5' },
@@ -17,7 +17,7 @@ export const addr2List = [
 export const addr3List = [
   { value: 'Хороо1', label: 'Хороо1' },
   { value: 'Хороо2', label: 'Хороо2' },
-  { value: 'Хороо3', label: 'Хороо3' },
+  { value: 'Хороо3', label: 'Хорооны нэр уртаар 3' },
   { value: 'Хороо4', label: 'Хороо4' },
   { value: 'Хороо5', label: 'Хороо5' },
 ];
