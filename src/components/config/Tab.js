a51d67499a63fdff86f5cc7cb824a8046666b348
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-19 15:31:17 +0800

    10.19_config_tab_add

--- src/components/config/Tab.js
+++ src/components/config/Tab.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Tabs } from 'antd';
 
+import { Additional } from './tabs';
+
 export function Tab(props){
   const { selectedKeys } = props;
   const items = [
-    { key: 'additional', children: 'Content 1' },
+    { key: 'additional', children: <Additional /> },
   ];
 
   return <Tabs activeKey={selectedKeys && selectedKeys[0]} items={items} />;
---
+++ src/components/config/tabs/Additional.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Additional(){
+  return (
+    <div>
+      Additional
+    </div>
+  );
+}
