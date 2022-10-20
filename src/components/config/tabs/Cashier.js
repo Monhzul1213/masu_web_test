0de2cdcb885893120c8739d2b4508a68c4685394
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-20 14:24:34 +0800

    10.20_cashier

--- src/components/config/tabs/Cashier.js
+++ src/components/config/tabs/Cashier.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Checkbox } from 'antd';
 import { useTranslation } from 'react-i18next';
 
 import { Overlay, IconButton, DynamicBSIcon } from '../../all';
@@ -7,11 +8,28 @@ export function Cashier(){
   const { t } = useTranslation();
   const [loading, setLoading] = useState(false);
   const [show, setShow] = useState(false);
+  const [data, setData] = useState([]);
+  const [checked, setChecked] = useState([]);
+
+  useEffect(() => {
+    setData([
+      { label: 'Бэлэн мөнгө', value: 'Бэлэн мөнгө' },
+      { label: 'Банкны карт', value: 'Банкны карт' },
+      { label: 'Онлайн гүйлгээ', value: 'Онлайн гүйлгээ', disabled: false },
+    ]);
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onClickAdd = () => {};
   
   const onClickDelete = () => {};
-  
+
+  const onChange = values => {
+    setChecked(values);
+    console.log('checked = ', values);
+  };
+
   return (
     <Overlay className='c_tab_cont' loading={loading}>
       <div className='ca_row'>
@@ -19,6 +37,13 @@ export function Cashier(){
           icon={<DynamicBSIcon name='BsPlusLg' className='ca_icon' />} onClick={onClickAdd} />
         {show && <IconButton className='ca_btn' text={t('page.delete')} id='ca_btn_delete'
           icon={<DynamicBSIcon name='BsTrash' className='ca_icon_del' />} onClick={onClickDelete} />}
+      </div>
+      <div className='ca_tab_back'>
+        <Checkbox.Group
+          className='ca_check'
+          value={checked}
+          options={data}
+          onChange={onChange} />
       </div>
     </Overlay>
   );
