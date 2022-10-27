527d15746510266744589b6551a7c2a83caa7114
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-27 16:45:38 +0800

    10.27_inventory

--- src/pages/invt/Inventory.js
+++ src/pages/invt/Inventory.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../../css/invt.css';
+import { Empty, Overlay } from '../../components/all';
+import { List } from '../../components/invt/inventory';
 
 export function Inventory(){
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  const onClickAdd = () => {
+
+  }
+ 
+  const emptyProps = { icon: 'MdOutlineShoppingBasket', type: 'inventory', onClickAdd };
+  // const listProps = { data, onClickAdd, onDelete, setLoading, setError, show, setShow, checked, setChecked, selected, setSelected };
+  const listProps = { };
+
   return (
     <div className='s_container'>
-      Inventory
+      <Overlay loading={loading}>
+        {data?.length ? <List {...listProps} /> : <Empty {...emptyProps} />}
+
+      </Overlay>
     </div>
   )
 }
