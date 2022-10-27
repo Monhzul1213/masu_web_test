d143744ea65cf61ec34036930ee7ed8fe89f9528
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-27 10:13:52 +0800

    10.27_category_item

--- src/components/invt/category/Item.js
+++ src/components/invt/category/Item.js
@@ -6,12 +6,14 @@ export function Item(props){
   const { item, checked, onCheck, lbl, onClick } = props;
 
   return (
-    <div className='cat_item' onClick={() => onClick(item)}>
+    <div className='cat_item'>
       <Check checked={checked} onClick={() => onCheck(item?.categoryId, checked)} />
-      <div className='cat_color' style={{backgroundColor: item?.colorCode}} />
-      <div className='cat_side'>
-        <p className='cat_title'>{item?.categoryName}</p>
-        <p className='cat_text'>{item?.items ?? 0} {lbl}</p>
+      <div className='cat_btn' onClick={() => onClick(item)}>
+        <div className='cat_color' style={{backgroundColor: item?.colorCode}} />
+        <div className='cat_side'>
+          <p className='cat_title'>{item?.categoryName}</p>
+          <p className='cat_text'>{item?.items ?? 0} {lbl}</p>
+        </div>
       </div>
     </div>
   )
