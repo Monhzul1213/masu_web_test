8a615d60e55385c4303cfe156efe061f7a9bbc06
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-27 10:11:37 +0800

    10.27_category_item

--- src/components/invt/category/Item.js
+++ src/components/invt/category/Item.js
@@ -8,10 +8,10 @@ export function Item(props){
   return (
     <div className='cat_item' onClick={() => onClick(item)}>
       <Check checked={checked} onClick={() => onCheck(item?.categoryId, checked)} />
-      <div className='cat_color' />
+      <div className='cat_color' style={{backgroundColor: item?.colorCode}} />
       <div className='cat_side'>
         <p className='cat_title'>{item?.categoryName}</p>
-        <p className='cat_text'>{item?.categoryCount ?? 0} {lbl}</p>
+        <p className='cat_text'>{item?.items ?? 0} {lbl}</p>
       </div>
     </div>
   )
