5a3c4b5157427556e9872c302f1373d619c0058b
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-27 15:33:07 +0800

    10.27_category_pagination

--- src/components/all/Pagination.js
+++ src/components/all/Pagination.js
@@ -9,51 +9,68 @@ export function Pagination(props){
   const [page, setPage] = useState(1);
   const [pageSize, setPageSize] = useState(2);
   const [count, setCount] = useState(1);
-  const pageRange = [2, 10, 25, 50, 100];
+  const pageRange = [10, 25, 50, 100];
 
   useEffect(() => {
-    setCount(Math.ceil(total / pageSize));
-    setStart((page - 1) * pageSize);
-    setEnd(page * pageSize);
+    onSizeChange(pageSize, total);
     return () => {};
-  }, [total, pageSize]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [total]);
 
-  const onClick = inc => {
-    let newPage = page + inc;
+  const onClick = newPage => {
     setPage(newPage);
     setStart((newPage - 1) * pageSize);
     setEnd(newPage * pageSize);
   }
 
-  const onChange = e => {
-    console.log(e);
+  const onSizeChange = (size, total1) => {
+    setPageSize(size);
+    setCount(Math.ceil(total1 / size));
+    let start = (page - 1) * size;
+    if(start >= total1){
+      setPage(1);
+      setStart(0);
+      setEnd(size);
+    } else {
+      setStart(start);
+      setEnd(page * size);
+    }
   }
 
-  const onBlur = e => {
+  const onChange = e => {
+    let value = parseInt(e.target.value);
+    if(isNaN(value)){
+      setPage('');
+    } else if(value <= count){
+      setPage(value);
+    }
+  }
 
+  const onBlur = () => {
+    onClick(page ? page : 1);
   }
 
   const onKeyDown = e => {
-
+    if(e?.key?.toLowerCase() === "enter") onClick(page ? page : 1);
   }
 
   return (
     <div className='pg_back'>
-      <button className='pg_btn' onClick={() => onClick(-1)}>
+      <button className='pg_btn' disabled={page <= 1} onClick={() => onClick(page - 1)}>
         <FiChevronLeft className='pg_icon' />
       </button>
       <div className='pg_input_back'>
         <input className='pg_input' value={page} onChange={onChange} onBlur={onBlur} onKeyDown={onKeyDown} />
         <p className='pg_text'>/ {count}</p>
       </div>
-      <button className='pg_btn' onClick={() => onClick(1)}>
+      <button className='pg_btn' disabled={page >= count} onClick={() => onClick(page + 1)}>
         <FiChevronRight className='pg_icon' />
       </button>
       <div className='pg_select_back'>
         <Select
           className='pg_select'
           value={pageSize}
-          onSelect={e => setPageSize(Number(e))}>
+          onSelect={e => onSizeChange(Number(e))}>
           {pageRange?.map(item => {
             return (<Option key={item} value={item}>{item}</Option>)
           })}
