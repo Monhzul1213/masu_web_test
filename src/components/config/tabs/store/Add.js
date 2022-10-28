b01438050f646c9b72b619508d934903a3bcf9a7
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-28 14:55:34 +0800

    10.28_shop_delete

--- src/components/config/tabs/store/Add.js
+++ src/components/config/tabs/store/Add.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, message } from 'antd';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { sendRequest } from '../../../../services';
-import { ButtonRow, ModalTitle, Overlay, Input, Error } from '../../../all';
+import { deleteRequest, sendRequest } from '../../../../services';
+import { ButtonRow, ModalTitle, Overlay, Input, Error, Confirm } from '../../../all';
 
 export function Add(props){
   const { visible, selected, closeModal } = props;
@@ -15,8 +15,20 @@ export function Add(props){
   const [descr, setDescr] = useState({ value: '' });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [open, setOpen] = useState(false);
   const { user, token }  = useSelector(state => state.login);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(selected){
+      setName({ value: selected?.name ?? '' });
+      setAddress({ value: selected?.address ?? '' });
+      setPhone({ value: selected?.phone ?? '' });
+      setDescr({ value: selected?.descr ?? '' });
+    }
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const onClickSave = async e => {
     e?.preventDefault();
@@ -39,8 +51,21 @@ export function Add(props){
     }
   }
 
-  const onClickDelete = () => {
+  const onClickDelete = () => setOpen(true);
 
+  const onDelete = async sure => {
+    setError(null);
+    setOpen(false);
+    if(sure){
+      setLoading(true);
+      const response = await dispatch(deleteRequest(user, token, 'Site/DeleteSite/' + selected?.siteId));
+      console.log(response);
+      if(response?.error) setError(response?.error);
+      else {
+        closeModal(true);
+        message.success(t('shop.delete_success'));
+      }
+    }
   }
 
   const nameProps = { value: name, setValue: setName, label: t('shop.name'), placeholder: t('shop.name1'), setError };
@@ -48,9 +73,11 @@ export function Add(props){
   const phoneProps = { value: phone, setValue: setPhone, label: t('shop.phone'), placeholder: t('shop.phone1'), setError };
   const descrProps = { value: descr, setValue: setDescr, label: t('shop.descr'), placeholder: t('shop.descr1'), setError, handleEnter: onClickSave };
   const btnProps = { onClickCancel: () => closeModal(), onClickSave, type: 'submit', show: selected ? true : false, onClickDelete };
+  const confirmProps = { open, text: t('page.delete_confirm'), confirm: onDelete };
 
   return (
     <Modal title={null} footer={null} closable={false} open={visible} centered={true} width={400}>
+      {open && <Confirm {...confirmProps} />}
       <Overlay loading={loading}>
         <div className='m_back'>
           <ModalTitle icon='MdStorefront' title={t(selected ? 'shop.edit' : 'shop.new')} isMD={true} />
