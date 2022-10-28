48f47a4e7c335f45f29e9a7502d17537d694c0dd
Bolor <bolor@Sukhnasans-iMac.local>
2022-10-28 17:18:14 +0800

    10.28_color

--- src/components/config/tabs/Cashier.js
+++ src/components/config/tabs/Cashier.js
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Checkbox } from 'antd';
-import { useTranslation } from 'react-i18next';
 
-import { Overlay, IconButton, DynamicBSIcon } from '../../all';
+import { Overlay, ButtonRowAdd } from '../../all';
 import { PayModal } from './cashiers';
 
 export function Cashier(){
-  const { t } = useTranslation();
   const [loading, setLoading] = useState(false);
   const [show, setShow] = useState(false);
   const [data, setData] = useState([]);
@@ -39,15 +37,13 @@ export function Cashier(){
   const closeModal = () => setVisible(null);
 
   const payProps = { visible: visible === 'pay', closeModal };
+  const addProps = { type: 'cashier', onClickAdd, show, onClickDelete };
 
   return (
     <Overlay className='c_tab_cont' loading={loading}>
       {visible === 'pay' && <PayModal {...payProps} />}
-      <div className='ca_row'>
-        <IconButton className='ca_btn' text={t('cashier.add')} id='ca_btn_add'
-          icon={<DynamicBSIcon name='BsPlusLg' className='ca_icon' />} onClick={onClickAdd} />
-        {show && <IconButton className='ca_btn' text={t('page.delete')} id='ca_btn_delete'
-          icon={<DynamicBSIcon name='BsTrash' className='ca_icon_del' />} onClick={onClickDelete} />}
+      <div style={{padding: 15}}>
+        <ButtonRowAdd {...addProps} />
       </div>
       <div className='ca_tab_back'>
         <Checkbox.Group
