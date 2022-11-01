a3025f123988ad5f28522833941b1a55a793deab
Bolor <bolor@Sukhnasans-iMac.local>
2022-11-01 17:14:07 +0800

    11.01_invt_add

--- src/pages/invt/InventoryAdd.js
+++ src/pages/invt/InventoryAdd.js
@@ -13,6 +13,10 @@ export function InventoryAdd(){
   const [categories, setCategories] = useState([]);
   const [descr, setDescr] = useState({ value: '' });
   const [unit, setUnit] = useState({ value: null });
+  const [price, setPrice] = useState({ value: '' });
+  const [cost, setCost] = useState({ value: '' });
+  const [invtID, setInvtID] = useState({ value: '' });
+  const [barcode, setBarcode] = useState({ value: '' });
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const { user, token }  = useSelector(state => state.login);
@@ -41,19 +45,35 @@ export function InventoryAdd(){
     data: categories, s_value: 'categoryId', s_descr: 'categoryName' };
   const descrProps = { value: descr, setValue: setDescr, label: t('inventory.descr1'), placeholder: t('inventory.descr1'), setError };
   const unitProps = { value: unit, setValue: setUnit, label: t('inventory.unit'), data: t('inventory.units'), setError };
+  const priceProps = { value: price, setValue: setPrice, label: t('inventory.price'), placeholder: t('inventory.price'), setError, inRow: true };
+  const costProps = { value: cost, setValue: setCost, label: t('inventory.cost'), placeholder: t('inventory.cost'), setError, inRow: true };
+  const skuProps = { value: invtID, setValue: setInvtID, label: t('inventory.sku'), placeholder: t('inventory.sku'), setError, inRow: true };
+  const barcodeProps = { value: barcode, setValue: setBarcode, label: t('inventory.barcode'), placeholder: t('inventory.barcode'), setError, inRow: true };
 
   return (
     <Overlay className='s_container1' loading={loading}>
       {error && <Error1 error={error} />}
-      <div className='ac_back'>
-        <div className='ac_row'>
-          <Input {...nameProps} />
-          <div className='gap' />
-          <Select {...categoryProps} />
+      <form>
+        <div className='ac_back'>
+          <div className='ac_row'>
+            <Input {...nameProps} />
+            <div className='gap' />
+            <Select {...categoryProps} />
+          </div>
+          <DescrInput {...descrProps} />
+          <Radio {...unitProps} />
+          <div className='ac_row' style={{marginTop: 10}}>
+            <Input {...priceProps} />
+            <div className='gap' />
+            <Input {...costProps} />
+          </div>
+          <div className='ac_row' style={{marginTop: 10}}>
+            <Input {...skuProps} />
+            <div className='gap' />
+            <Input {...barcodeProps} />
+          </div>
         </div>
-        <DescrInput {...descrProps} />
-        <Radio {...unitProps} />
-      </div>
+      </form>
     </Overlay>
   )
 }
