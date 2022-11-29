75bd44500bf265da1e34ff9eaf4c6c83f71f3155
Bolor <bolor@Sukhnasans-iMac.local>
2022-11-29 12:17:08 +0800

    11.29_receipt_table

--- src/components/report/receipt/List.js
+++ src/components/report/receipt/List.js
@@ -39,7 +39,7 @@ export function List(props){
   return (
     <div>
       <div style={{overflowX: 'scroll'}}>
-        <div id='paging' style={{overflowY: 'scroll', maxHeight}}>
+        <div id='paging' style={{overflowY: 'scroll', maxHeight, minWidth: 720}}>
           <Table {...tableProps} />
         </div>
       </div>
