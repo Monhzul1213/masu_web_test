e41ba1649e8159e42ac100530b9458cd06fb64b5
Bolor <bolor@Sukhnasans-iMac.local>
2022-11-30 11:11:32 +0800

    11.30_receipt_header_design

--- src/components/report/receipt/Header.js
+++ src/components/report/receipt/Header.js
@@ -18,7 +18,7 @@ export function Header(props){
 
   const exportProps = { label: t('page.export'), className: 'rp_list_select', data: t('report_receipt.export') };
   const width = showSearch ? 0 : 50;
-  const width1 = !showSearch ? 0 : (size?.width > 465 ? 320 : (size?.width - 30));
+  const width1 = !showSearch ? 0 : (size?.width > 495 ? 320 : (size?.width - 60));
   const style = { width, overflow: 'hidden', transition: 'width 0.2s ease-in' };
   const searchProps = { className: 'ih_search', name: 'AiOutlineSearch', onClick: onClickSearch };
   const inputProps = { showSearch, setShowSearch, handleEnter, search, setSearch, width: width1, className: 'rp_list_search_back' };
