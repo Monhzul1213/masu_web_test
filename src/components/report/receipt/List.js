e350653370512645bacce6a8944a4b9c673bb9d5
Bolor <bolor@Sukhnasans-iMac.local>
2022-11-30 18:07:59 +0800

    11.30_receipt

--- src/components/report/receipt/List.js
+++ src/components/report/receipt/List.js
@@ -24,7 +24,7 @@ export function List(props){
       { Header: t('time.t_site'), accessor: 'sale.siteName' },
       { Header: t('time.t_emp'), accessor: 'sale.cashierName' },
       // { Header: t('time.t_emp'), accessor: 'sale.empName' },
-      // { Header: t('report_receipt.t_user'), accessor: 'sale.custName' },
+      { Header: t('report_receipt.t_user'), accessor: 'sale.custName' },
       { Header: t('report_receipt.t_type'), accessor: 'sale.salesTypeName' },
       {
         Header: <div style={{textAlign: 'right'}}>{t('report_receipt.t_total')}</div>, accessor: 'sale.totalSalesAmount', customStyle: { width: 100 },
