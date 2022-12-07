87ff64a49a2512e02ab4d6728a1c14e973012e64
Bolor <bolor@Sukhnasans-iMac.local>
2022-12-07 14:24:36 +0800

    12.07_review_card

--- src/components/report/review/Graph.js
+++ src/components/report/review/Graph.js
@@ -5,31 +5,31 @@ import '../../../css/report.css';
 import { formatNumber } from '../../../helpers';
 
 export function Graph(props){
-  const { tab, setTab } = props;
+  const { tab, setTab, total } = props;
   const { t } = useTranslation();
   
   const Card = props => {
-    const { label, value1, value2, value3, isPos, index } = props;
-    const color = !value3 ? 'var(--text-color)' : ((isPos && value3 > 0) || (!isPos && value3 < 0)) ? 'var(--success-color)' : 'var(--danger-color)';
+    const { label, value, index } = props;
+    // const color = !value3 ? 'var(--text-color)' : ((isPos && value3 > 0) || (!isPos && value3 < 0)) ? 'var(--success-color)' : 'var(--danger-color)';
     const style = index === tab ?  { borderColor: 'var(--config-color)' } : { };
 
     return (
-      <button className='rr_card' style={style} onClick={() => setTab(index)}>
+      <div className='rr_card' style={style} onClick={() => setTab(index)}>
         <p className='rr_card_label'>{t('report_review.' + label)}</p>
-        <p className='rr_card_value'>₮{formatNumber(value1)}</p>
-        <p className='rr_card_value1' style={{ color }}>₮{formatNumber(value2)} ({value3 ?? 0}%)</p>
-      </button>
+        <div className='rr_card_value'>₮{formatNumber(value)}</div>
+        {/* <p className='rr_card_value1' style={{ color }}>₮{formatNumber(value2)} ({value3 ?? 0}%)</p> */}
+      </div>
     )
   }
 
   return (
     <div className='rr_graph_cont'>
       <div className='rr_card_back'>
-        <Card label='c_sales' index={0} isPos={true} />
-        <Card label='c_refund' index={1} isPos={false} />
-        <Card label='c_discount' index={2} isPos={false} />
-        <Card label='c_net' index={3} isPos={true} />
-        <Card label='c_profit' index={4} isPos={true} />
+        <Card label='c_sales' index={0} isPos={true} value={total?.sales} />
+        <Card label='c_refund' index={1} isPos={false} value={total?.refund} />
+        <Card label='c_discount' index={2} isPos={false} value={total?.discount} />
+        <Card label='c_net' index={3} isPos={true} value={total?.net} />
+        <Card label='c_profit' index={4} isPos={true} value={total?.profit} />
       </div>
     </div>
   )
--- src/pages/report/Review.js
+++ src/pages/report/Review.js
@@ -2,25 +2,31 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { withSize } from 'react-sizeme';
+import moment from 'moment';
 
 import '../../css/invt.css';
 import { Error1, Overlay } from '../../components/all';
 import { Filter } from '../../components/report/receipt';
 import { Graph } from '../../components/report/review';
+import { getList } from '../../services';
 
 function Screen(props){
   const { size } = props;
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [tab, setTab] = useState(0);
+  const [period, setPeriod] = useState('D');
+  const [filter, setFilter] = useState('');
+  const [total, setTotal] = useState(null);
   const { user, token }  = useSelector(state => state.login);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   useEffect(() => {
     if(user?.msRole?.webViewSalesReport !== 'Y') navigate({ pathname: '/' });
     else {
-      // let query = '?BeginDate=' + moment()?.startOf('month')?.format('yyyy.MM.DD') + '&EndDate=' + moment()?.format('yyyy.MM.DD');
-      // getData(query);
+      let query = '?BeginDate=' + moment()?.startOf('month')?.format('yyyy.MM.DD') + '&EndDate=' + moment()?.format('yyyy.MM.DD');
+      getData(query);
     }
     return () => {};
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -28,10 +34,34 @@ function Screen(props){
 
   const getData = async query => {
     console.log(query);
+    setError(null);
+    setLoading(true);
+    let api = 'Sales/GetSalesSummary' + (query ?? '') + '&SearchPeriod=' + period;
+    console.log(api);
+    const response = await dispatch(getList(user, token, api));
+    console.log(response?.data);
+    if(response?.error) setError(response?.error);
+    else {
+      let sales = 0, refund = 0, discount = 0, net = 0, profit = 0;
+      response?.data?.forEach(item => {
+        sales += item?.totalSalesAmt;
+        refund += item?.totalReturnAmt;
+        discount += item?.totalDiscAmt;
+        net += item?.totalNetSalesAmt;
+        profit += item?.totalProfitAmt;
+      });
+      setTotal({ sales, refund, discount, net, profit });
+      // setData(response?.data);
+      // tab === -1
+      //   ? setFilteredData(response?.data)
+      //   : setFilteredData(response?.data?.filter(item => item?.sale?.salesType === tab));
+    }
+    setLoading(false);
+    setFilter(query);
   }
   
   let filterProps = { onSearch: getData, size, setError };
-  let graphProps = { tab, setTab };
+  let graphProps = { tab, setTab, total };
 
   return (
     <div className='s_container_r'>
