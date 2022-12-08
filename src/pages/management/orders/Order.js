cd849dbc2ec99bb660d4d666d4a3ff5222a7e2c6
Bolor <bolor@Sukhnasans-iMac.local>
2022-12-08 15:40:15 +0800

    12.08_order

--- src/pages/management/orders/Order.js
+++ src/pages/management/orders/Order.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import '../../../css/order.css';
+import { Overlay } from '../../../components/all';
 
 export function Order(){
+  const [loading, setLoading] = useState(false);
+  
   return (
-    <div>
-      Order
+    <div className='s_container_i'>
+      <Overlay loading={loading}>
+
+      </Overlay>
     </div>
   );
 }
+
+/**
+ * <div className='s_container_i'>
+      <Overlay loading={loading}>
+        {error && <Error1 error={error} />}
+        {!data?.length && !filtering ? <Empty {...emptyProps} /> :
+          <SizeMe>{({ size }) => 
+            <div className='i_list_cont' id='invt_list'>
+              <Header {...headerProps} size={size} />
+              {!data?.length ? <Empty1 {...emptyProps} /> : <List {...listProps} size={size} />}
+            </div>
+          }</SizeMe>
+        }
+      </Overlay>
+    </div>
+ */
--- src/pages/report/Review.js
+++ src/pages/report/Review.js
@@ -63,12 +63,14 @@ function Screen(props){
     let newData = [];
     if(period === 'D'){
       let diff = date[1]?.diff(date[0], 'days');
-      for (let index = 0; index <= diff; index++) {
-        let salesDate = moment(date[0]).add(index, 'days')?.format('yyyy-MM-DDT00:00:00');
-        let exists = data?.findIndex(res => salesDate === res?.salesDate);
-        if(exists !== -1) newData.push(data[exists]);
-        else newData.push({ salesDate, totalSalesAmt: 0, totalReturnAmt: 0, totalDiscAmt: 0, totalNetSalesAmt: 0, totalProfitAmt: 0 });
-      }
+      if(diff <= 60){
+        for (let index = 0; index <= diff; index++) {
+          let salesDate = moment(date[0]).add(index, 'days')?.format('yyyy-MM-DDT00:00:00');
+          let exists = data?.findIndex(res => salesDate === res?.salesDate);
+          if(exists !== -1) newData.push(data[exists]);
+          else newData.push({ salesDate, totalSalesAmt: 0, totalReturnAmt: 0, totalDiscAmt: 0, totalNetSalesAmt: 0, totalProfitAmt: 0 });
+        }
+      } else newData = data;
     } else if(period === 'H'){
       for (let index = 0; index <= 23; index++) {
         let exists = data?.findIndex(res => index === res?.salesDate);
