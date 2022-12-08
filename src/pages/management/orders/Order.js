cb3401e80c985548dd1d94f56aa674288e3be875
Bolor <bolor@Sukhnasans-iMac.local>
2022-12-08 15:49:37 +0800

    12.08_order

--- src/pages/management/orders/Order.js
+++ src/pages/management/orders/Order.js
@@ -1,32 +1,34 @@
 import React, { useState } from 'react';
+import { SizeMe } from 'react-sizeme';
 
 import '../../../css/order.css';
-import { Overlay } from '../../../components/all';
+import { Overlay, Error1, Empty } from '../../../components/all';
 
 export function Order(){
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const [filtering, setFiltering] = useState(false);
+
+  const onClickAdd = row => {
+
+  }
+  
+  const emptyProps = { icon: 'MdOutlineArticle', type: 'order', noDescr: true, onClickAdd };
   
   return (
     <div className='s_container_i'>
-      <Overlay loading={loading}>
-
-      </Overlay>
-    </div>
-  );
-}
-
-/**
- * <div className='s_container_i'>
       <Overlay loading={loading}>
         {error && <Error1 error={error} />}
         {!data?.length && !filtering ? <Empty {...emptyProps} /> :
           <SizeMe>{({ size }) => 
             <div className='i_list_cont' id='invt_list'>
-              <Header {...headerProps} size={size} />
-              {!data?.length ? <Empty1 {...emptyProps} /> : <List {...listProps} size={size} />}
+              {/* <Header {...headerProps} size={size} /> */}
+              {/* {!data?.length ? <Empty1 {...emptyProps} /> : <List {...listProps} size={size} />} */}
             </div>
           }</SizeMe>
         }
       </Overlay>
     </div>
- */
+  );
+}
