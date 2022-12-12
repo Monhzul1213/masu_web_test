b21aac6ab5c2857650ed64359d722f0a25714fac
Bolor <bolor@Sukhnasans-iMac.local>
2022-12-12 19:26:00 +0800

    12.12_order_screen_menu

--- src/components/management/order/screen/Menu.js
+++ src/components/management/order/screen/Menu.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { MdChevronLeft } from 'react-icons/md';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
-import { DynamicAIIcon, IconButton, Button, Dropdown } from '../../../all';
+import { IconButton, Button, Dropdown } from '../../../all';
 
 export function Menu(props){
   const { order } = props;
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
-  const onClick = () => console.log('onClick back');
-  const onPressApprove = () => {};
+  const onClick = () => navigate(-1);
+  const onPressApprove = () => console.log(order);
   const onPressReceive = () => {};
+  const onPressEdit = () => {};
   const onPressSend = () => {};
 
-  const backProps = { className: 'ps_back_btn', text: t('order.back'), icon: <DynamicAIIcon name='AiOutlineLeft' className='ps_back_icon' />, onClick };
+  const backProps = { className: 'ps_back_btn', text: t('order.back'), icon: <MdChevronLeft className='ps_back_icon' />, onClick };
   const approveProps = { className: 'ps_btn', text: t('order.approve'), onClick: onPressApprove };
-  const receiveProps = { className: 'ps_btn', text: t('order.approve'), onClick: onPressReceive };
+  const receiveProps = { className: 'ps_btn', text: t('order.receive'), onClick: onPressReceive };
+  const editProps = { className: 'ps_btn', text: t('order.edit'), onClick: onPressEdit };
   const sendProps = { className: 'ps_btn', text: t('order.send'), onClick: onPressSend };
-  const menu1Props = { className: 'ps_dropdown', label: t('order.more'), data: t('order.menu1') };
+  const menu1Props = { className: 'ps_btn', label: t('order.more'), data: t('order.menu1') };
 
   return (
     <div className='ps_menu_back'>
@@ -24,6 +29,7 @@ export function Menu(props){
       <div style={{flex: 1}} />
       <Button {...approveProps} />
       <Button {...receiveProps} />
+      <Button {...editProps} />
       <Button {...sendProps} />
       <Dropdown {...menu1Props} />
     </div>
