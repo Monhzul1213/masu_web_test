24ed03c04d824a49b7a548cb050e1aaac19c6354
Bolor <bolor@Sukhnasans-iMac.local>
2022-12-19 17:52:47 +0800

    12.19_config_additional

--- src/components/config/add/Item.js
+++ src/components/config/add/Item.js
@@ -9,7 +9,7 @@ export function Item(props){
   return (
     <div className='a_item'>
       <div className='a_item_text'>
-        <p className='a_item_title'>{item?.title}1ddd</p>
+        <p className='a_item_title'>{item?.title}</p>
         <p className='a_item_sub_title'>{item?.sub_title}<button className='a_item_more'>{more}</button></p>
       </div>
       <Switch className='a_item_check' checked={item?.checked} onChange={onChange} />
