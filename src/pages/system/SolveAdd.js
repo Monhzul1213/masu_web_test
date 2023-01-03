cdec3606e40137796c4fb6627443faf64412059c
Bolor <bolor@Sukhnasans-iMac.local>
2023-01-03 13:01:39 +0800

    01.03_solve_add

--- src/pages/system/SolveAdd.js
+++ src/pages/system/SolveAdd.js
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+import { message } from 'antd';
 
 import '../../css/invt.css';
 import '../../css/config.css';
-import { getList } from '../../services';
+import { getList, sendRequest } from '../../services';
 import { ButtonRowConfirm, Error1, Overlay, Prompt } from '../../components/all';
 import { Main, List } from '../../components/system/solve/add';
 
@@ -48,6 +49,7 @@ export function SolveAdd(){
     setLoading(true);
     let api = 'Merchant/VatRequest/GetSolvedRequests?RequestID=' + requestId;
     let response = await dispatch(getList(user, token, api));
+    console.log(response);
     setLoading(false);
     if(response?.error) setError(response?.error);
     else {
@@ -71,7 +73,22 @@ export function SolveAdd(){
     let lengthValid = names?.length === items?.length;
     let nameValid = names.length === new Set(names).size;
     if(status?.value !==4 || (lengthValid && nameValid)){
-      let data = {...request, status: status?.value, descr: notes?.value, requestItem: items };
+      let msVatRequestFiles = items?.map(item => {
+        return { 
+          rowStatus: 'U',
+          siteId: item?.siteId,
+          terminalId: item?.terminalID,
+          fileName: item?.fileName ?? '',
+          fileraw: item?.fileRaw ?? {}
+        }
+      });
+      let data = {
+        requestID: request?.requestId,
+        descr: notes?.value,
+        status: status?.value,
+        rowStatus: 'U',
+        msVatRequestFiles
+      }
       return data;
     }
     else if(!lengthValid) setError(t('tax.file_error'));
@@ -79,26 +96,34 @@ export function SolveAdd(){
     return false;
   }
 
+  const onLoad = () => {
+    setError(null);
+    setLoading(true);
+    setEdited(false);
+  }
+
+  const onError = (err, edited) => {
+    setError(err);
+    setEdited(edited);
+    setLoading(false);
+  }
+
+  const onSuccess = (msg, saved) => {
+    if(msg) message.success(t(msg));
+    if(saved) setSaved(true);
+    setLoading(false);
+  }
+
   const onClickSave = async () => {
     let data = validateData();
     console.log(data);
     if(data){
-      setSaved(true);
+      onLoad();
+      const response = await dispatch(sendRequest(user, token, 'Merchant/VatRequestSolve', data));
+      console.log(response);
+      if(response?.error) onError(response?.error, true);
+      else onSuccess(t('tax.solve_success'), true);
     }
-    // if(data){
-    //   onLoad();
-    //   const response = await dispatch(sendRequest(user, token, 'Employee/Modify', data));
-    //   if(response?.error) onError(response?.error, true);
-    //   else {
-    //     if(selected && selected?.email?.toLowerCase() === user?.mail?.toLowerCase()){
-    //       let pass = password?.value ? password?.value : user?.password;
-    //       const response1 = await dispatch(apiLogin(mail?.value, pass));
-    //       if(response1?.error) onError(response1?.error, true);
-    //       else onSuccess(t('employee.add_success'), true);
-    //     } else 
-    //       onSuccess(t('employee.add_success'), true);
-    //   }
-    // }
   }
   
   let mainProps = { setError, setEdited, regNo, name, checked, status, setStatus, notes, setNotes };
@@ -120,143 +145,3 @@ export function SolveAdd(){
     </Overlay>
   );
 }
-
-/*
-import { apiLogin, getList, sendRequest } from '../../services';
-import { validateEmail } from '../../helpers';
-import { ButtonRowConfirm, Error1, Overlay, Prompt } from '../../components/all';
-import { CardMain } from '../../components/emp/employee/add';
-import { CardSite } from '../../components/invt/modifier/add';
-import { CardEmpty } from '../../components/invt/inventory/add';
-
-export function EmployeeAdd(){
-  const [name, setName] = useState({ value: '' });
-  const [mail, setMail] = useState({ value: '' });
-  const [password, setPassword] = useState({ value: '' });
-  const [phone, setPhone] = useState({ value: '' });
-  const [role, setRole] = useState({ value: null });
-  const [code, setCode] = useState({ value: '' });
-  const [sites, setSites] = useState([]);
-  const [checked, setChecked] = useState(true);
-  const [selected, setSelected] = useState(null);
-  const [show, setShow] = useState(false);
-
-
-  
-
-  
-  
-  const onLoad = () => {
-    setError(null);
-    setLoading(true);
-    setEdited(false);
-  }
-
-  const onError = (err, edited) => {
-    setError(err);
-    setEdited(edited);
-    setLoading(false);
-  }
-
-  const onSuccess = (msg, saved) => {
-    if(msg) message.success(t(msg));
-    if(saved) setSaved(true);
-    setLoading(false);
-  }
-
-  
-
-  const getEmployee = async (empCode, sites1) => {
-    onLoad();
-    let response = await dispatch(getList(user, token, 'Employee/GetEmployees?EmpCode=' + empCode));
-    if(response?.error) onError(response?.error, false);
-    else {
-      onSuccess();
-      let emp = response && response?.data && response?.data[0];
-      if(emp){
-        setSelected(emp);
-        setShow(emp?.isOwner !== 'Y' && emp?.email?.toLowerCase() !== user?.mail?.toLowerCase());
-        setName({ value: emp.empName ?? '' });
-        setMail({ value: emp.email ?? '' });
-        setPhone({ value: emp.phone ?? '' });
-        setCode({ value: emp.poS_PIN ?? '' });
-        setRole({ value: emp?.roleId });
-        setChecked(emp?.useAllSite === 'Y');
-        sites1?.forEach(item => {
-          let exists = emp?.empsites?.filter(si => si.siteId === item.siteId)[0];
-          item.checked = exists ? true : false;
-          item.rowStatus = exists ? 'U' : 'I';
-        });
-        setSites(sites1);
-      }
-    }
-  }
-
-  const getSites = async () => {
-    onLoad();
-    const response = await dispatch(getList(user, token, 'Site/GetSite'));
-    if(response?.error){
-      onError(response?.error, false);
-      return false;
-    } else {
-      response?.data?.forEach(item => {
-        item.checked = true;
-        item.rowStatus = 'I';
-      });
-      setSites(response?.data);
-      onSuccess();
-      return response?.data;
-    }
-  }
-
-  const validateData = () => {
-    let codeLength = 4, passwordLength = 8, nameLength = 2, phoneLength = 8;
-    let isEmailValid = validateEmail(mail?.value);
-    let pin = code?.value?.replace(/[ _]/g, '');
-    let isCodeValid = pin?.length === 4;
-    let isPasswordValid = (selected && !password?.value) || password?.value?.length >= passwordLength;
-    let isNameValid = name?.value?.length >= nameLength;
-    let isPhoneValid = !phone?.value || phone?.value?.length >= phoneLength;
-    if(isNameValid && isEmailValid && (role?.value || role?.value === 0) && isCodeValid && isPasswordValid && isPhoneValid){
-      let employeeSites = [];
-      sites?.forEach(item => {
-        if(item?.checked) employeeSites.push({ siteID: item?.siteId, rowStatus: item?.rowStatus ?? 'I' });
-        else if(item?.rowStatus === 'U') employeeSites.push({ siteID: item?.siteId, rowStatus: 'D' });
-      });
-      let data = [{ empCode: selected?.empCode ?? -1, empName: name?.value, email: mail?.value, password: password?.value, employeeSites,
-        phone: phone?.value, roleID: role?.value, rowStatus: selected ? 'U' : 'I', useAllSite: checked ? 'Y' : 'N', poS_PIN: pin
-      }];
-      return data;
-    } else {
-      if(!name?.value) setName({ value: '', error: t('error.not_empty') });
-      else if(!isNameValid) setName({ value: name?.value, error: ' ' + nameLength + t('error.longer_than') });
-      if(!mail?.value) setMail({ value: '', error: t('error.not_empty') });
-      else if(!isEmailValid) setMail({ value: mail?.value, error: t('error.be_right') });
-      if(!(role?.value || role?.value === 0)) setRole({ value: role?.value, error: t('error.not_empty') });
-      if(!code?.value) setCode({ value: '', error: t('error.not_empty') });
-      else if(!isCodeValid) setCode({ value: code?.value, error: codeLength + t('error.must_be') });
-      if(!password?.value && !selected) setPassword({ value: '', error: t('error.not_empty') });
-      else if(!isPasswordValid) setPassword({ value: password?.value, error: ' ' + passwordLength + t('error.longer_than') });
-      if(!isPhoneValid) setPhone({ value: phone?.value, error: ' ' + phoneLength + t('error.longer_than') })
-      return false;
-    }
-  }
-
- 
-
-  const onClickDelete = async () => {
-    onLoad();
-    let data = [{...selected, rowStatus: 'D', roleID: selected?.roleId, password: '', poS_PIN: '', employeeSites: []}];
-    const response = await dispatch(sendRequest(user, token, 'Employee/Modify', data));
-    if(response?.error) onError(response?.error, true);
-    else onSuccess(t('employee.delete_success'), true);
-  }
-
-  let mainProps = { setError, setEdited, name, setName, mail, setMail, password, setPassword, phone, setPhone, role, setRole, code, setCode, selected,
-    isOwner: selected?.isOwner === 'Y' };
-  let siteProps = { data: sites, setData: setSites, setEdited, checked, setChecked, id: 'ea_back', label: 'employee' };
-  let siteEmptyProps = { title: 'inventory.sites', icon: 'MdStorefront', route: '/config/store', btn: 'shop.add', id: 'ea_back' };
-
-  
-}
-*/
