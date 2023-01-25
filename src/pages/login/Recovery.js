bae1c41e4d5a9ea674ed8435394b5958b80c1bef
Bolor Purevdorj <bolorpurevdorj@Bolors-MacBook-Pro.local>
2023-01-25 10:18:45 +0800

    01.24_recovery

--- src/pages/login/Recovery.js
+++ src/pages/login/Recovery.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { message } from 'antd';
 import { useSearchParams, Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
-import emailjs from '@emailjs/browser';
 
 import '../../css/login.css';
 import '../../css/config.css';
-import { config } from '../../helpers';
 import { apiRecovery } from '../../services';
 import { login_image } from '../../assets';
 import { Button, Error, Input } from '../../components/all';
@@ -29,25 +26,12 @@ export function Recovery(){
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const sendEmail = async to => {
-    const link = config?.domain + '/confirm?mail=' + to;
-    const templateParams = { to, link };
-    return emailjs.send('service_k7osau81','template_3dlaawl', templateParams, 'q2YX3XN0cT2C8g_Ni')
-      .then((response) => {
-        message.success(t('login.success'));
-      }, (err) => {
-        console.log(err);
-        setError(t('login.send_error'));
-      }
-    );
-  }
-
   const onClick = async e => {
     setLoading(true);
     setError(null);
     const response = await dispatch(apiRecovery(value?.value));
     if(response?.error) setError(response?.error);
-    else await sendEmail();
+    else setSent(true);
     setLoading(false);
   }
 
@@ -64,6 +48,9 @@ export function Recovery(){
         <Button className='l_btn' text={t('login.send')} onClick={onClick} loading={loading} />
         {error && <Error error={error} />}
         <div className='co_gap' />
+        {sent && <div className='co_sent_back'>
+          <p className='co_sent_text'>{t('login.sent')}</p>
+        </div>}
         <div className='l_center_row'>
           <Link className='l_link' to='/'>{t('login.back')}</Link>
         </div>
