aa52eebe4fb0bacaf2450077c731846f044365d8
Bolor Purevdorj <bolorpurevdorj@Bolors-MacBook-Pro.local>
2023-01-31 17:49:04 +0800

    01.31_receipt

--- src/pages/configs/Document.js
+++ src/pages/configs/Document.js
@@ -1,29 +1,74 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { withSize } from 'react-sizeme';
 
 import { getList } from '../../services';
-import { ButtonRowAdd, Empty, Error1, Overlay } from '../../components/all';
-import { Add, List } from '../../components/config/store';
+import { Empty, Error1, Overlay, PlainSelect } from '../../components/all';
 
 function Card(props){
+  const { size } = props;
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [sites, setSites] = useState([]);
+  const [site, setSite] = useState(null);
+  const { user, token }  = useSelector(state => state.login);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const emptyProps = { icon: 'MdOutlineReceiptLong', type: 'document', noDescr: true };//onClickAdd
+  useEffect(() => {
+    getData();
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getData = async () => {
+    setError(null);
+    setLoading(true);
+    const res1 = await getSites();
+    if(res1 || res1 === 0) await getBill(res1);
+    setLoading(false);
+  }
+
+  const getSites = async () => {
+    const response = await dispatch(getList(user, token, 'Site/GetSite'));
+    if(response?.error) {
+      setError(response?.error);
+      return false;
+    } else {
+      setSites(response?.data);
+      let first = response?.data && response?.data[0];
+      let siteID = first ? first?.siteId : false;
+      return siteID;
+    }
+  }
+
+  const getBill = async siteID => {
+    setSite(siteID);
+    const response = await dispatch(getList(user, token, 'Site/GetBill?SiteID=' + siteID));
+    if(response?.error) setError(response?.error);
+    else {
+      
+    }
+  }
+
+  const onClickShop = () => navigate('/config/store');
+
+  const width = size?.width >= 720 ? 720 : size?.width;
+  const emptyProps = { icon: 'MdOutlineReceiptLong', type: 'document', onClickAdd: onClickShop };
+  const siteProps = { value: site, setValue: getBill, data: sites, s_value: 'siteId', s_descr: 'name',
+    className: 'r_select', classBack: 'do_select' };
 
   return (
     <div className='store_tab' style={{flex: 1}}>
-      <Empty {...emptyProps} />
-
-      {/* {visible && <Add {...modalProps} />} */}
       <Overlay loading={loading}>
-        {/* {error && <Error1 error={error} />} */}
-        {/* {!data?.length ? <div style={{ width }}><Empty {...emptyProps} /></div> :
+        {error && <Error1 error={error} />}
+        {!sites?.length ? <Empty {...emptyProps} /> :
           <div className='mo_container' style={{ width }}>
-            <ButtonRowAdd {...addProps} />
-            <List {...listProps} />
+            <PlainSelect {...siteProps} />
+            {/* {!data?.length ? <Empty1 {...emptyProps} /> : <List {...listProps} />} */}
           </div>
-        } */}
+        }
       </Overlay>
     </div>
   );
@@ -31,11 +76,9 @@ function Card(props){
 /*
 function Card(props){
   const { size } = props;
-  const [error, setError] = useState(null);
   const [data, setData] = useState([]);
   const [visible, setVisible] = useState(false);
   const [item, setItem] = useState(null);
-  const { user, token }  = useSelector(state => state.login);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -64,7 +107,6 @@ function Card(props){
     if(toGet) getData();
   }
 
-  const width = size?.width >= 720 ? 720 : size?.width;
   const emptyProps = { icon: 'MdStorefront', type: 'shop', noDescr: true, onClickAdd };
   const modalProps = { visible, closeModal, selected: item };
   const addProps = { type: 'shop', onClickAdd };
