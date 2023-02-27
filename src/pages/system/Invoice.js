5f4f1f4e63fec59c665b0879e3b83165124c34c4
Bolor Purevdorj <Bolor@Ultimates-iMac.local>
2023-02-27 11:18:04 +0800

    02.27_system_invoice_data

--- src/pages/system/Invoice.js
+++ src/pages/system/Invoice.js
@@ -4,27 +4,47 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate, createSearchParams } from 'react-router-dom';
 import moment from 'moment';
 
-// import '../../css/invt.css';
-// import { getList } from '../../services';
+import '../../css/invt.css';
+import { getList } from '../../services';
 import { Empty1, Error1, Overlay } from '../../components/all';
-import { Header } from '../../components/system/invoice/list';
-// import { Header, List } from '../../components/system/solve/list';
+import { Header, List } from '../../components/system/invoice/list';
 
 export function Invoice(){
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [data, setData] = useState([]);
+  const { user, token }  = useSelector(state => state.login);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if(user?.isAdmin){
+      let query = '?BeginDate=' + moment()?.startOf('month')?.format('yyyy.MM.DD') +
+        '&EndDate=' + moment()?.format('yyyy.MM.DD');
+      getData(query);
+    } else 
+      navigate({ pathname: '/' });
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const getData = async query => {
-    // setError(null);
-    // setLoading(true);
-    // let api = 'Merchant/VatRequest/GetSolvedRequests' + (query ?? '');
-    // const response = await dispatch(getList(user, token, api));
-    // if(response?.error) setError(response?.error);
-    // else setData(response?.data);
-    // setLoading(false);
+    setError(null);
+    setLoading(true);
+    let api = 'Txn/GetInvoice' + (query ?? '');
+    const response = await dispatch(getList(user, token, api));
+    if(response?.error) setError(response?.error);
+    else setData(response?.data);
+    setLoading(false);
+  }
+
+  const onClickAdd = row => {
+    navigate({ pathname: 'solve_add', search: createSearchParams({ requestId: row?.requestId }).toString() });
   }
 
   const headerProps = { setError, onSearch: getData };
+  const emptyProps = { icon: 'MdReceipt', type: 'time', onClickAdd, noDescr: true };
+  const listProps = { data, setData, onClickAdd };
 
   return (
     <div className='s_container_i'>
@@ -33,43 +53,10 @@ export function Invoice(){
         <SizeMe>{({ size }) => 
           <div className='i_list_cont' id='solve_list'>
             <Header {...headerProps} size={size} />
-            {/*
-            {!data?.length ? <Empty1 {...emptyProps} /> : <List {...listProps} size={size} />} */}
+            {!data?.length ? <Empty1 {...emptyProps} /> : <List {...listProps} size={size} />}
           </div>
         }</SizeMe>
       </Overlay>
     </div>
   );
 }
-
-// export function Solve(){
-//   const [data, setData] = useState([]);
-//   const { user, token }  = useSelector(state => state.login);
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     if(user?.isAdmin){
-//       let query = '?BeginDate=' + moment()?.startOf('month')?.format('yyyy.MM.DD') +
-//         '&EndDate=' + moment()?.format('yyyy.MM.DD');
-//       getData(query);
-//     } else 
-//       navigate({ pathname: '/' });
-    
-//     return () => {};
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
- 
-
-//   const onClickAdd = row => {
-//     navigate({ pathname: 'solve_add', search: createSearchParams({ requestId: row?.requestId }).toString() });
-//   }
-  
-//   const emptyProps = { icon: 'MdReceipt', type: 'time', onClickAdd, noDescr: true };
-//   const listProps = { data, setData, onClickAdd };
-
-//   return (
-   
-//   );
-// }
