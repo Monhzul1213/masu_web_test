4f9f99277b3709356aef9b5abae3e34795a54324
bb1213 <boloroo.chanchan@gmail.com>
2023-03-10 15:06:15 +0800

    03.10_qpay

--- src/components/menu/Install.js
+++ src/components/menu/Install.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AiOutlineAppstore } from 'react-icons/ai';
 import { useTranslation } from 'react-i18next';
+
+import { getOS } from '../../helpers';
 
 export function Install(props){
   const { collapsed } = props;
   const { t } = useTranslation();
+
+  useEffect(() => {
+    console.log(getOS());
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <a className='mi_btn' href='https://app.masu.mn/files/MasuInstaller.exe' download='MasuInstaller.exe'>
