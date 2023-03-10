0685408cd0565ab13a45112386bd58044220966f
bb1213 <boloroo.chanchan@gmail.com>
2023-03-10 17:44:06 +0800

    03.10_menu_install_open

--- src/components/menu/Install.js
+++ src/components/menu/Install.js
@@ -1,21 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AiOutlineAppstore } from 'react-icons/ai';
 import { useTranslation } from 'react-i18next';
+import OpenApp from "react-open-app";
 
 import { getOS } from '../../helpers';
 
 export function Install(props){
   const { collapsed } = props;
   const { t } = useTranslation();
+  const [type, setType] = useState(null);
 
   useEffect(() => {
-    console.log(getOS());
+    setType(getOS());
     return () => {};
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return (
-    <a className='mi_btn' href='https://app.masu.mn/files/MasuInstaller.exe' download='MasuInstaller.exe'>
+  return type === 'iOS' || type === 'Android' ?  (
+    <OpenApp
+      href="masuapp://"
+      android="masuapp://"
+      ios="masuapp://">
+      <div className='mi_btn'>
+        <AiOutlineAppstore className='mi_icon' />
+        {!collapsed && <p className='mi_text'>{t('menu.install')}</p>}
+      </div>
+    </OpenApp>
+  ) : (
+    <a className='mi_btn' href='MASU:'>
       <AiOutlineAppstore className='mi_icon' />
       {!collapsed && <p className='mi_text'>{t('menu.install')}</p>}
     </a>
