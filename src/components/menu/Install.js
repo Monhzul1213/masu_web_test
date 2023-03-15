9ec3be31078d439f75f9e46c3f843d76c5b53a04
bb1213 <boloroo.chanchan@gmail.com>
2023-03-15 09:39:42 +0800

    03.15_menu_install

--- src/components/menu/Install.js
+++ src/components/menu/Install.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { AiOutlineAppstore } from 'react-icons/ai';
 import { useTranslation } from 'react-i18next';
-import OpenApp from "react-open-app";
+// import OpenApp from "react-open-app";
+import customProtocolCheck from "custom-protocol-check";
 
 import { getOS } from '../../helpers';
 
@@ -16,20 +17,61 @@ export function Install(props){
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return type === 'iOS' || type === 'Android' ?  (
-    <OpenApp
-      href="masuapp://"
-      android="masuapp://"
-      ios="masuapp://">
-      <div className='mi_btn'>
-        <AiOutlineAppstore className='mi_icon' />
-        {!collapsed && <p className='mi_text'>{t('menu.install')}</p>}
-      </div>
-    </OpenApp>
-  ) : (
-    <a className='mi_btn' href='MASU:'>
+  const onClick = async () => {
+    let scheme = (type === 'iOS' || type === 'Android') ? "masuapp://" : "MASU:"
+    let url = type === 'Android'
+      ? "https://play.google.com/store/apps/details?id=com.masupos"
+      : type === 'iOS'
+        ? "https://apps.apple.com/mn/app/masupos/id1671694304"
+        : "https://app.masu.mn/files/MasuInstaller.exe"
+    let now = new Date().valueOf();
+    if(type === 'Android'){
+      customProtocolCheck(scheme,
+        () => { window.open(url) },
+        () => { },
+        1500
+      );
+    } else if(type === 'iOS') {
+      setTimeout(function () {
+        if (new Date().valueOf() - now > 1500) return;
+          window.open(url)
+        }, 1200);
+      window.location = scheme;
+    } else {
+      setTimeout(function () {
+        if(new Date().valueOf() - now > 1500) return;
+          const link = document.createElement('a');
+          link.href = url;
+          document.body.appendChild(link);
+          link.click();
+          link.parentNode.removeChild(link);
+        }, 1200);
+      window.location = scheme;
+    }
+    return;
+  }
+
+  return (
+    <button className='mi_btn' onClick={onClick}>
       <AiOutlineAppstore className='mi_icon' />
       {!collapsed && <p className='mi_text'>{t('menu.install')}</p>}
-    </a>
-  );
+    </button>
+  )
+
+  // return type === 'iOS' || type === 'Android' ?  (
+  //   <OpenApp
+  //     href="masuapp://"
+  //     android="masuapp://"
+  //     ios="masuapp://">
+  //     <div className='mi_btn'>
+  //       <AiOutlineAppstore className='mi_icon' />
+  //       {!collapsed && <p className='mi_text'>{t('menu.install')}</p>}
+  //     </div>
+  //   </OpenApp>
+  // ) : (
+  //   <a className='mi_btn' href='MASU:'>
+  //     <AiOutlineAppstore className='mi_icon' />
+  //     {!collapsed && <p className='mi_text'>{t('menu.install')}</p>}
+  //   </a>
+  // );
 }
