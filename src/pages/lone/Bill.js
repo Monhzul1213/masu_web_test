120312030c8d175aef1132d7c536b9a28a340853
bb1213 <boloroo.chanchan@gmail.com>
2023-04-24 16:18:39 +0800

    04.24_bill_get

--- src/pages/lone/Bill.js
+++ src/pages/lone/Bill.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSearchParams } from 'react-router-dom';
 
-export function Bill(props){
+import { getService } from '../../services';
+import { Error1, Overlay } from '../../components/all';
+
+export function Bill(){
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [searchParams] = useSearchParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    getData();
+    return () => {};
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getData = async () => {
+    setLoading(true);
+    setError(null);
+    let billno = searchParams?.get('billno');
+    let api = 'Sales/GetSalesBill?data=' + encodeURIComponent(billno);
+    let response = await dispatch(getService(api, 'GET'));
+    if(response?.error) setError(response?.error);
+    else setData(response?.data);
+    setLoading(false);
+  }
+
   return (
-    <div>
-      Bill
-    </div>
+    <Overlay className='i_container' loading={!loading}>
+      {error && <Error1 error={error} />}
+    </Overlay>
   )
 }
