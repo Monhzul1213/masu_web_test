905f1c3dba3834068d0526b08145c9f6b273433d
bb1213 <boloroo.chanchan@gmail.com>
2023-05-11 15:45:04 +0800

    05.11_order_header

---
+++ src/components/lone/order/Info.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { QRCode } from 'react-qrcode-logo';
+import { useTranslation } from 'react-i18next';
+import moment from 'moment';
+
+export function Info(props){
+  const { header } = props;
+  const { t } = useTranslation();
+
+  return (
+    <div className='bo_header'>
+      <p className='bo_powered'>{t('order_bill.powered')}</p>
+      <p className='bo_no'>{t('order_bill.title')}: #{header?.orderNo}</p>
+      <div className='bo_row'>
+        <div className='bo_col'>
+          <p className='bo_label'>{t('order_bill.ordered')}: <span className='bo_value'>{header?.custName}</span></p>
+          <p className='bo_label'>{t('order_bill.reg_no')}: <span className='bo_value'>{header?.custRegNo}</span></p>
+          <p className='bo_label'>{t('order_bill.employee')}: <span className='bo_value'>{header?.custRegNo}</span></p>
+          <p className='bo_label'>{t('order_bill.date')}: <span className='bo_value'>{moment(header?.orderDate).format('yyyy.MM.DD')}</span></p>
+          <p className='bo_label'>{t('order_bill.req_date')}: <span className='bo_value'>{header?.reqDate}</span></p>
+          {header?.notes ? <p className='bo_label'>{t('order_bill.descr')}: <span className='bo_value'>{header?.notes}</span></p> : null}
+        </div>
+        <QRCode
+          value="https://github.com/gcoro/react-qrcode-logo"
+          logoImage={require('../../../assets/logo.png')}
+          size={135}
+          fgColor='#6f0ad2'
+          logoWidth={48}
+          logoHeight={16} />
+      </div>
+    </div>
+  );
+}
--- src/pages/lone/Order.js
+++ src/pages/lone/Order.js
@@ -5,7 +5,7 @@ import { useSearchParams } from 'react-router-dom';
 import '../../css/bill.css';
 import { getService } from '../../services';
 import { DynamicBSIcon, Error1, Overlay } from '../../components/all';
-// import { Header, Info, Items, QR, Total } from '../../components/lone/bill';
+import { Info } from '../../components/lone/order';
 
 export function Order(){
   const [loading, setLoading] = useState(false);
@@ -22,20 +22,17 @@ export function Order(){
   }, []);
 
   const getData = async () => {
-    // setLoading(true);
-    // setError(null);
-    // let billno = searchParams?.get('billno');
-    // let api = 'Sales/GetSalesBill?data=' + encodeURIComponent(billno);
-    // let response = await dispatch(getService(api, 'GET'));
-    // if(response?.error) setError(response?.error);
-    // else {
-    //   let header = response?.data?.retdata?.sales && response?.data?.retdata?.sales[0];
-    //   if(header) header.pureAmount = (header.totalSalesAmount ?? 0) - (header.totalVatAmount ?? 0) - (header.totalNhatamount ?? 0);
-    //   if(header) header.paidAmount = (header.totalCashAmount ?? 0) + (header.totalNonCashAmount ?? 0);
-    //   setHeader(header);
-    //   setDetail(response?.data?.retdata?.salesitem);
-    // }
-    // setLoading(false);
+    setLoading(true);
+    setError(null);
+    let orderno = searchParams?.get('orderno');
+    let api = 'Txn/GetOrderBill?data=' + encodeURIComponent(orderno);
+    let response = await dispatch(getService(api, 'GET'));
+    if(response?.error) setError(response?.error);
+    else {
+      setHeader(response?.data?.retdata?.order);
+      setDetail(response?.data?.retdata?.orderitem);
+    }
+    setLoading(false);
   }
 
   return (
@@ -44,7 +41,8 @@ export function Order(){
         {error && <Error1 error={error} />}
         {!header ? <DynamicBSIcon name='BsReceipt' className='bl_empty' /> :
           <div>
-            {/* <Info header={header} />
+            <Info header={header} />
+            {/*
             <Header />
             <Items detail={detail} />
             <Total header={header} />
