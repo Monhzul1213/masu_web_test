11149a7f3d3a0cf4b2579e872a7d5c0df6d4928b
Zulaa <zulaa@users-iMac.local>
2023-08-11 14:00:38 +0800

    08-11-list

--- src/src1/components/system/notification/list/List.js
+++ src/src1/components/system/notification/list/List.js
@@ -36,8 +36,8 @@ export function List(props){
   }, [i18n?.language]);
 
   useEffect(() => {
-    if(size?.width >= 660) setMaxHeight('calc(100vh - var(--header-height) - var(--page-padding) * 3 - 7px - 51px - 10px - 37px)');
-    else setMaxHeight('calc(100vh - var(--header-height) - var(--page-padding) * 3 - 7px - 110px - 10px - 37px)');
+    if(size?.width >= 660) setMaxHeight('calc(100vh - var(--header-height) - var(--page-padding) * 3 - 7px - 51px - 10px - 107px)');
+    else setMaxHeight('calc(100vh - var(--header-height) - var(--page-padding) * 3 - 7px - 110px - 10px - 107px)');
     return () => {};
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [size?.width]);
