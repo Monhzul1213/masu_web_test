7c99a25b6f2b1674da16c08a4e757cea36ec93e4
bb1213 <boloroo.chanchan@gmail.com>
2024-04-12 11:54:15 +0800

    04.12_bonus_filter

---
+++ src/components/loyalty/bonus/list/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export function List(props){
+  const { } = props;
+  // COMMENT
+
+  return (
+    <div>
+      List
+    </div>
+  );
+}
--- src/pages/loyalty/Bonus.js
+++ src/pages/loyalty/Bonus.js
@@ -1,63 +1,18 @@
-import React, { useState } from 'react';
-import { SizeMe } from 'react-sizeme';
-
-import '../../css/invt.css';
-import { Error1, Overlay } from '../../components/all';
-import { Filter } from '../../components/loyalty/bonus/list';
-
-export function Bonus(){
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
-  const onSearch = async query => {
-    console.log(query);
-    // comment
-    // setError(null);
-    // setLoading(true);
-    // let api = 'Site/GetCoupon' + query ?? '';
-    // const response = await dispatch(getList(user, token, api));
-    // if(response?.error) setError(response?.error);
-    // else {
-    //   response?.data?.coupon?.forEach(item=> {
-    //     item.date = moment(item?.beginDate)?.format('yyyy.MM.DD') + '-' + moment(item?.endDate)?.format('yyyy.MM.DD')
-    //   })
-    //   setData(response?.data?.coupon);
-    // }
-    // setLoading(false);
-  }
-
-  const onClickAdd = () => {};//comment navigate('coupon_add');
-
-  const headerProps = { onClickAdd, setError, onSearch, setError };
-
-  return (
-    <div className='s_container_i'>
-      <Overlay loading={loading}>
-        {error && <Error1 error={error} />}
-        <SizeMe>{({ size }) => 
-          <div className='i_list_cont' id='invt_list'>
-            {/* comment */}
-            <Filter {...headerProps} size={size} />
-            {/* {!data?.length ? <Empty1 icon='MdOutlineArticle' /> : <List {...listProps} size={size} />} */}
-          </div>
-        }</SizeMe>
-      </Overlay>
-    </div>
-  );
-}
-
-/**
-comment
 import React, { useEffect, useState } from 'react';
+import { SizeMe } from 'react-sizeme';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import moment from 'moment';
 
-import { getList } from '../../../services';
-import { Empty1, Error1, Overlay } from '../../../components/all';
-import { Filter, List } from '../../components/loyalty/coupon/list';
+import '../../css/invt.css';
+import { getList } from '../../services';
+import { Error1, Overlay } from '../../components/all';
+import { Filter, List } from '../../components/loyalty/bonus/list';
+import { Empty1 } from '../../components/all';
 
-export function Coupon(){
+export function Bonus(){
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [data, setData] = useState([]);
   const { user, token }  = useSelector(state => state.login);
   const dispatch = useDispatch();
@@ -73,13 +28,38 @@ export function Coupon(){
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  
+  const onSearch = async query => {
+    setError(null);
+    setLoading(true);
+    let api = 'Site/GetBonus' + query ?? '';
+    const response = await dispatch(getList(user, token, api));
+    if(response?.error) setError(response?.error);
+    else {
+    // COMMENT
+    //   response?.data?.coupon?.forEach(item=> {
+    //     item.date = moment(item?.beginDate)?.format('yyyy.MM.DD') + '-' + moment(item?.endDate)?.format('yyyy.MM.DD')
+    //   })
+    //   setData(response?.data?.coupon);
+    }
+    setLoading(false);
+  }
 
+  const onClickAdd = () => navigate('bonus_add');
 
+  const headerProps = { onClickAdd, setError, onSearch, setError };
   const listProps = { data, onClickAdd };
 
   return (
-    
+    <div className='s_container_i'>
+      <Overlay loading={loading}>
+        {error && <Error1 error={error} />}
+        <SizeMe>{({ size }) => 
+          <div className='i_list_cont' id='invt_list'>
+            <Filter {...headerProps} size={size} />
+            {!data?.length ? <Empty1 icon='MdOutlineArticle' /> : <List {...listProps} size={size} />}
+          </div>
+        }</SizeMe>
+      </Overlay>
+    </div>
   );
 }
- */
