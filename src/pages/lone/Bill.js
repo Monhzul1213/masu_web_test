159859a0a9d47d26c7cdd5023ed0493950a0a76f
Zulaa <zulaa@users-iMac.local>
2024-08-23 09:39:05 +0800

    08_23_bill_back

--- src/pages/lone/Bill.js
+++ src/pages/lone/Bill.js
@@ -44,7 +44,7 @@ export function Bill(){
 
   return (
     <Overlay loading={loading}>
-      <div className='bl_back'>
+      <div className='bill_back'>
         {error && <Error1 error={error} />}
         {!header ? <DynamicBSIcon name='BsReceipt' className='bl_empty' /> :
           <div>
