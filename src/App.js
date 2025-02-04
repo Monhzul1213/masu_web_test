0079ecec31436f665edb0976dfbfed8841c87114
Zulaa <zulaa@users-iMac.local>
2025-02-04 11:45:45 +0800

    02-04

--- src/App.js
+++ src/App.js
@@ -95,7 +95,6 @@ export function App(){
                 <Route path='/bill' element={<Bill />} />
                 <Route path='/order' element={<OrderBill />} />
                 <Route path='/statement' element={<InvoiceSend />} />
-                <Route path='/invoice' element={<InvoicePrint />} />
               </Routes>
             </Layout>
           </Layout>
@@ -167,7 +166,6 @@ export function App(){
                 <Route path='/system/partner' element={<PartnerInfo />} />
                 <Route path='/bill' element={<Bill />} />
                 <Route path='/order' element={<OrderBill />} />
-                <Route path='/invoice' element={<InvoicePrint />} />
                 <Route path='/statement' element={<InvoiceSend />} />
                 <Route path='/report/report_orList' element={<SalesOrder />} />
                 <Route path='/report/terms' element={<Terms />} />
